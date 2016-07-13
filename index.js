6c9aa82ff785b4d0ed02ea8e68cb8509ab3906ea
Aashish Welling <aashishwelling@gmail.com>
2016-07-12 23:57:34 -0400

    Switched to new format

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
 
 });
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: 'right',
+        afterLoad: function() {
+            var self = $(this);
+            self.find(".typed").typed({
+                stringsElement: self.find('.typed-strings')
+            });
+        },
+        onLeave: function(){
+            var self = $(this);
+            setTimeout(function() {
+                self.find(".typed").typed({
+                    strings: ['']
+                })
+            }, 500);
+        }
+    });
+});
