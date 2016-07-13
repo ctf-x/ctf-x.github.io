ba10665543cb82acab4cd843559c03546f67a718
Aashish Welling <aashishwelling@gmail.com>
2016-07-13 00:53:48 -0400

    Stopped resetting between pages

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
 
 });
 $(document).ready(function() {
+    var visited = [];
     $('#fullpage').fullpage({
         navigation: true,
         navigationPosition: 'right',
-        afterLoad: function() {
+        afterLoad: function(x, idx) {
+            console.log(visited);
+            if($.inArray(idx, visited) !== -1)
+                return;
+            visited.push(idx);
             var self = $(this);
             self.find(".typed").typed({
                 typeSpeed: 1,
